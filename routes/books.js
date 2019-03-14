aceebd9e6f7f22a91c8f5d60c998353284e3a56a
nictengkk <nictengkk@gmail.com>
2019-03-14 14:44:54 +0800

    create route for posting a book, update test for book post route

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 const books = [
-  { name: "abc", author: "abcde" },
-  { name: "def", author: "defgh" },
-  { name: "ghi", author: "ghijk" }
+  { id: "1", name: "abc", author: "abcde" },
+  { id: "2", name: "def", author: "defgh" },
+  { id: "3", name: "ghi", author: "ghijk" }
 ];
 
-router.get("/", (req, res) => {
-  res.send(books);
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.status(200);
+    res.json(books);
+  })
+  .post((req, res) => {
+    const book = req.body;
+    book.name = "jkl";
+    book.author = "jklmn";
+    book.id = "4";
+    res.status(201);
+    res.json(book);
+  });
 
+router
+  .route("/:id")
+  .put((req, res) => {
+    req.query.id;
+    res.status(202);
+  })
+  .delete((req, res) => {
+    res.status(202);
+  });
 module.exports = router;
--- tests/routes/books.test.js
+++ tests/routes/books.test.js
@@ -2,18 +2,26 @@ const request = require("supertest");
 const app = require("../../app");
 
 const booksSampleData = [
-  { name: "abc", author: "abcde" },
-  { name: "def", author: "defgh" },
-  { name: "ghi", author: "ghijk" }
+  { id: "1", name: "abc", author: "abcde" },
+  { id: "2", name: "def", author: "defgh" },
+  { id: "3", name: "ghi", author: "ghijk" }
 ];
 
 describe("Books", () => {
   let route = "/books";
-  test("should display list of books ", done => {
+  test("should display list of books", () => {
     return request(app)
       .get(route)
       .expect(200)
       .expect("Content-type", /json/)
-      .expect(booksSampleData, done);
+      .expect(booksSampleData);
+  });
+  test("should add a book", () => {
+    return request(app)
+      .post(route)
+      .set("Accept", "application/json")
+      .send({ id: "4", name: "jkl", author: "jklmn" })
+      .expect(201)
+      .expect({ id: "4", name: "jkl", author: "jklmn" });
   });
 });
