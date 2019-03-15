d861bb89ce6e88680df6d92a6b16ce0a82b8bbe5
nictengkk <nictengkk@gmail.com>
2019-03-15 15:31:37 +0800

    prepared file for heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require("./app");
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server is running on https://localhost:${port}`);
+const inProduction = app.listen(port, () => {
+  if (process.env.NODE_ENV === "production") {
+    console.log(`Server is running on heroku with PORT number ${port}`);
+  } else {
+    console.log(`Server is running on https://localhost:${port}`);
+  }
 });
