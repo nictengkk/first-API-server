7e16d405867d8bc38cdf7a05edc1e748ca970a6b
nictengkk <nictengkk@gmail.com>
2019-03-18 16:37:16 +0800

    set up connection to db and functions to log console messages if error

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/books-db");
+//mongodb.connect() if you are not using mongoose wrapper
 const port = process.env.PORT || 8080;
 
-const inProduction = app.listen(port, () => {
-  if (process.env.NODE_ENV === "production") {
-    console.log(`Server is running on heroku with PORT number ${port}`);
-  } else {
-    console.log(`Server is running on https://localhost:${port}`);
-  }
+const db = mongoose.connection;
+
+//db.on (on is an event listener)
+db.on("error", err => {
+  console.error("Unable to connect to database, err:", err);
+});
+db.on("connected", () => {
+  console.log("Successfully connected to database");
+});
+
+//db.once runs callback function only once.
+db.once("open", () => {
+  const inProduction = app.listen(port, () => {
+    if (process.env.NODE_ENV === "production") {
+      console.log(`Server is running on heroku with PORT number ${port}`);
+    } else {
+      console.log(`Server is running on https://localhost:${port}`);
+    }
+  });
 });
