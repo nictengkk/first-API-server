1e2269e417efa110782aad64b23926f550021c26
nictengkk <nictengkk@gmail.com>
2019-03-20 10:08:00 +0800

    update tests for remaining CRUD methods

--- tests/routes/books.test.js
+++ tests/routes/books.test.js
@@ -2,12 +2,7 @@ const request = require("supertest");
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const app = require("../../app");
-
-const booksSampleData = [
-  { id: "1", title: "The Intelligent Investor", author: "Benjamin Graham" },
-  { id: "2", title: "Way of the Wolf", author: "Jordan Belfort" },
-  { id: "3", title: "Beating the Street", author: "Peter Lynch" }
-];
+const Book = require("../../models/books");
 
 const route = (params = "") => {
   const path = "/api/v1/books";
@@ -16,17 +11,29 @@ const route = (params = "") => {
 
 describe("Books", () => {
   let mongoServer;
+  let db;
 
-  //this is done because each test sends in a data set into the original database, which makes it dirty. We want the test db to appear and disappear after each test during dev mode.
+  beforeEach(async () => {
+    await Book.insertMany([
+      {
+        title: "The Intelligent Investor",
+        author: "Benjamin Graham"
+      },
+      { title: "Way of the Wolf", author: "Jordan Belfort" },
+      { title: "Beating the Street", author: "Peter Lynch" }
+    ]);
+  });
+
   beforeAll(async () => {
     jest.setTimeout(120000); //in case each default test timeout of 5s is not enough for the async to complete.
     mongoServer = new MongoMemoryServer(); //start mongodb in test mode
     const mongoUri = await mongoServer.getConnectionString();
     await mongoose.connect(mongoUri);
-    // to kill the memory of storing a temp database
-    // db.dropCollection(("books", err)=>{
-    //   console.error(err)
-    // })
+    db = mongoose.connection;
+  });
+
+  afterEach(async () => {
+    await db.dropCollection("books");
   });
 
   afterAll(async () => {
@@ -36,28 +43,97 @@ describe("Books", () => {
 
   describe("[GET] Search for books", () => {
     test("should display list of books", () => {
+      const expectedBooks = [
+        {
+          title: "The Intelligent Investor",
+          author: "Benjamin Graham"
+        },
+        { title: "Way of the Wolf", author: "Jordan Belfort" },
+        { title: "Beating the Street", author: "Peter Lynch" }
+      ];
+
       return request(app) //access the app
         .get(route())
         .expect(200)
         .expect("Content-type", /json/)
-        .expect(booksSampleData);
+        .then(res => {
+          const books = res.body;
+          expect(books.length).toBe(3);
+          books.forEach((book, index) => {
+            expect(book).toEqual(expect.objectContaining(expectedBooks[index]));
+          });
+        });
     });
 
-    test("should display book(s) of different author and title queries", () => {
+    test("returns books matching the title query", () => {
       return request(app)
         .get(route())
-        .query({ author: "Peter Lynch", title: "Way of the Wolf" })
+        .query({ title: "The Intelligent Investor" })
+        .expect("content-type", /json/)
         .expect(200)
-        .expect([
-          { id: "2", title: "Way of the Wolf", author: "Jordan Belfort" },
-          { id: "3", title: "Beating the Street", author: "Peter Lynch" }
-        ]);
+        .then(res => {
+          const book = res.body[0];
+          expect(book.title).toEqual("The Intelligent Investor");
+        });
     });
+
+    test("returns books matching the author query", () => {
+      const expectedBooks = [
+        {
+          title: "The Intelligent Investor",
+          author: "Benjamin Graham"
+        },
+        { title: "Way of the Wolf", author: "Jordan Belfort" },
+        { title: "Beating the Street", author: "Peter Lynch" }
+      ];
+
+      return request(app)
+        .get(route())
+        .query({ author: "Jordan Belfort" })
+        .expect("content-type", /json/)
+        .expect(200)
+        .then(res => {
+          const books = res.body;
+          books.forEach((book, index) => {
+            expect(book.title).toBe(expectedBooks[index].title);
+            expect(book.author).toBe(expectedBooks[index].author);
+          });
+        });
+    });
+
+    // xtest("should display book(s) of different author and title queries", async () => {
+    //   const expectedBooks = [
+    //     {
+    //       title: "Beating the Street",
+    //       author: "Peter Lynch"
+    //     },
+    //     { title: "Way of the Wolf", author: "Jordan Belfort" }
+    //   ];
+
+    //   await request(app)
+    //     .get(route())
+    //     .query({ author: "Peter Lynch", title: "Way of the Wolf" })
+    //     .expect(200)
+
+    //     .then(res => {
+    //       const books = res.body;
+    //       books.forEach((book, index) => {
+    //         expect(book).toEqual(expect.objectContaining(expectedBooks[index]));
+    //       });
+    //     });
+    // });
   });
 
   describe("[POST] Creates a new book entry", () => {
-    test("denies access when no token is provided", () => {
-      return request(app)
+    const expectedBooks = [
+      {
+        title: "The Intelligent Investor",
+        author: "Benjamin Graham"
+      },
+      { title: "Way of the Wolf", author: "Jordan Belfort" }
+    ];
+    test("denies access when no token is provided", async () => {
+      await request(app)
         .post(route())
         .set("Content-type", "application/json")
         .send({ title: "Rich Dad Poor Dad", author: "Robert Kiyosaki" })
@@ -66,8 +142,8 @@ describe("Books", () => {
         });
     });
 
-    test("denies access when incorrect token is given", () => {
-      return request(app)
+    test("denies access when incorrect token is given", async () => {
+      await request(app)
         .post(route())
         .set("Authorization", "Bearer some-invalid-token")
         .set("Content-type", "application/json")
@@ -78,38 +154,29 @@ describe("Books", () => {
         });
     });
 
-    test("grants access when correct token is given", () => {
-      return request(app)
+    test("grants access when correct token is given", async () => {
+      const res = await request(app)
         .post(route())
         .set("Authorization", "Bearer my-awesome-token")
         .set("Content-type", "application/json")
         .send({ title: "Rich Dad Poor Dad", author: "Robert Kiyosaki" })
-        .expect(201)
-        .then(res => {
-          expect(res.body).toEqual(
-            expect.objectContaining({
-              title: "Rich Dad Poor Dad",
-              author: "Robert Kiyosaki"
-            })
-          );
-          // toEqual({
-          //   _id: expect.any(String),
-          //   __v: expect.any(Number),
-          //   title: "Rich Dad Poor Dad",
-          //   author: "Robert Kiyosaki"
-          // });
-        });
+        .expect(201);
+
+      expect(res.body.title).toBe("Rich Dad Poor Dad");
+      expect(res.body.author).toBe("Robert Kiyosaki");
+
+      const book = await Book.findOne({ title: "Rich Dad Poor Dad" });
+      expect(book.title).toBe("Rich Dad Poor Dad");
     });
   });
 
   describe("[PUT] Edits an existing book entry", () => {
-    test("denies access when no token is provided", () => {
-      const id = 3;
+    test("denies access when no token is provided", async () => {
+      const { _id } = await Book.findOne({ title: "The Intelligent Investor" });
       return request(app)
-        .put(route(id))
+        .put(route(_id))
         .send({
-          id: `${id}`,
-          name: "One up on Wall Street",
+          title: "One up on Wall Street",
           author: "Peter Lynch"
         })
         .ok(res => res.status === 403)
@@ -118,14 +185,13 @@ describe("Books", () => {
         });
     });
 
-    test("denies access when invalid token is provided", () => {
-      const id = 3;
+    test("denies access when invalid token is provided", async () => {
+      const { _id } = await Book.findOne({ title: "The Intelligent Investor" });
       return request(app)
-        .put(route(id))
+        .put(route(_id))
         .set("Authorization", "Bearer some-invalid-token")
         .send({
-          id: `${id}`,
-          name: "One up on Wall Street",
+          title: "One up on Wall Street",
           author: "Peter Lynch"
         })
         .ok(res => res.status === 403)
@@ -134,56 +200,96 @@ describe("Books", () => {
         });
     });
 
-    test("Successfully edit a book's title with the correct token", () => {
-      const id = 3;
+    test("Successfully edit a book's title with the correct token", async () => {
+      //const {_id} = await Book.findOne({title: "The Intelligent Investor"})
+      const _id = await request(app)
+        .get(route())
+        .query({ title: "The Intelligent Investor" })
+        .expect(200)
+        .then(res => {
+          const { _id } = res.body[0];
+          return _id;
+        });
+
+      // const res = await request(app)
+      //   .put(route(_id))
+      //   .set("Authorization", "Bearer my-awesome-token")
+      //   .send({
+      //     title: "Security Analysis",
+      //     author: "Benjamin Graham"
+      //   })
+      //   .expect(202);
+      // expect(res.body).toEqual(
+      //   expect.objectContaining({
+      //     title: "Security Analysis",
+      //     author: "Benjamin Graham"
+      //   })
+      // );
       return request(app)
-        .put(route(id))
+        .put(route(_id))
         .set("Authorization", "Bearer my-awesome-token")
         .send({
-          id: `${id}`,
-          name: "One up on Wall Street",
-          author: "Peter Lynch"
+          title: "Security Analysis",
+          author: "Benjamin Graham"
         })
         .expect(202)
-        .expect({
-          id: `${id}`,
-          name: "One up on Wall Street",
-          author: "Peter Lynch"
+        .then(res => {
+          expect(res.body).toEqual(
+            expect.objectContaining({
+              title: "Security Analysis",
+              author: "Benjamin Graham"
+            })
+          );
         });
     });
 
     test("Fails to edit book as no such id despite valid token", () => {
-      const id = 100;
+      const _id = "5c8fb5c41529bf25dcba41a7";
       return request(app)
-        .put(route(id))
+        .put(route(_id))
         .set("Authorization", "Bearer my-awesome-token")
         .send({
-          id: `${id}`,
-          name: "One up on Wall Street",
+          title: "One up on Wall Street",
           author: "Peter Lynch"
         })
         .catch(res => {
-          expect(res.status).toBe(403);
+          expect(res.status).toBe(500);
         });
     });
   });
 
   describe("[DELETE] Deletes an existing book entry", () => {
-    test("Successfully delete a book entry", () => {
-      const id = 3;
-      return request(app)
-        .delete(route(id))
-        .expect(202);
+    test("denies access when no token is provided", async () => {
+      const { _id } = await Book.findOne({ title: "The Intelligent Investor" });
+      await request(app)
+        .delete(route(_id))
+        .ok(res => res.status === 403)
+        .then(res => {
+          expect(res.status).toBe(403);
+        });
     });
 
-    test("Fails to delete a book as it does not exist", () => {
-      const id = 100;
+    test("Successfully delete a book entry", async () => {
+      const { _id } = await Book.findOne({ title: "Way of the Wolf" });
 
-      return request(app)
-        .delete(route(id))
-        .ok(res => res.status === 400)
+      await request(app)
+        .delete(route(_id))
+        .set("Authorization", "Bearer my-awesome-token")
+        .expect(202);
+
+      const book = await Book.findOne({ _id });
+      expect(book).toBe(null);
+    });
+
+    test("Fails to delete a book as it does not exist", async () => {
+      const _id = "5c8fb5c41529bf25dcba41a7";
+
+      await request(app)
+        .delete(route(_id))
+        .set("Authorization", "Bearer my-awesome-token")
+        .ok(res => res.status === 404)
         .then(res => {
-          expect(res.status).toBe(400);
+          expect(res.status).toBe(404);
         });
     });
   });
