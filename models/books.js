28720113c9c45b401ed628231d9aa69046b3099b
nictengkk <nictengkk@gmail.com>
2019-03-20 10:06:53 +0800

    update schema to include validation and timestamps

--- models/books.js
+++ models/books.js
@@ -3,10 +3,23 @@
 //model naming convention is Singular, collection naming convention is plural
 
 const mongoose = require("mongoose");
-const bookSchema = new mongoose.Schema({
-  title: String,
-  author: String
-});
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 const Book = mongoose.model("Book", bookSchema);
 
 module.exports = Book;
